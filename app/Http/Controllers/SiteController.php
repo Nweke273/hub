a17675c10747f343109177da895178534d5055bf
Nweke273 <chisom5710@gmail.com>
2022-10-08 18:14:40 +0100

    another test

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -3,28 +3,100 @@
 namespace App\Http\Controllers;
 
 use App\Models\Order;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 
 class SiteController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('/');
     }
 
-    public function send(Request $request)
-    {
-            Order::create([
-            'order' => json_encode($request->all())
+    public function send(Request $request){
+        try {
+            $client = new \GuzzleHttp\Client();
+            $url = 'https://api.loyverse.com/v1.0/receipts';
+            $response = $client->request('POST', $url, [
+                'headers' => [
+                    'Content-Type' => 'application/json',
+                    'Authorization' => 'Bearer 9112882d8417477d99176ac7eeb5e0bc',
+                ],
+                'body' => json_encode([
+
+
+                    "store_id" => "948f7a3b-3ebf-4bdc-8a42-9305ebd610c8",
+                    "order" => "ORDER-103885",
+                    "customer_id" => str::uuid("34228379"),
+                    "source" => "My app",
+                    "receipt_date" => "2020-06-23T08:35:47.047Z",
+                    "total_discounts" => [
+                        [
+                            "id" => "50f4e245-d221-448d-943a-7346c21cd82b",
+                            "percentage" => 15,
+                            "scope" => "LINE_ITEM"
+                        ],
+                        [
+                            "id" => "23a64c4f-c6e5-43cc-a017-b11a6ee32448",
+                            "scope" => "RECEIPT"
+                        ],
+                        [
+                            "id" => "77d2bf40-7b12-11ea-bc55-0242ac130003",
+                            "money_amount" => 5,
+                            "scope" => "RECEIPT"
+                        ],
+                    ],
+                    "line_items" => [
+                        [
+                            "variant_id" => "06929667-cc44-4bbb-b226-6758285d7033",
+                            "quantity" => 2
+                        ],
+                        [
+                            "variant_id" => "706e2626-3329-45f8-98d7-0e1dbcbcb9d9",
+                            "quantity" => 1,
+                            "price" => 100,
+                            "cost" => 50,
+                            "line_note" => "Some line note",
+                            "line_discounts" => [
+                                [
+                                    "id" => "50f4e245-d221-448d-943a-7346c21cd82b"
+                                ],
+                            ],
+                            "line_taxes" => [
+                                [
+                                    "id" => "a94d8606-7268-11ea-bde9-1269e7c5a22d"
+                                ],
+                                [
+                                    "id" => "365972a1-d5f9-449d-bc61-4328cf0e62cb"
+                                ]
+                            ],
+                            "line_modifiers" => [
+                                [
+                                    "modifier_option_id" => "0a7cdf41-c75b-11ea-f4ff-abc85f017314",
+                                    "price" => 2
+                                ]
+                            ]
+                        ]
+                    ],
+                    "note" => "Some note for the receipt",
+                    "payments" => [
+                        [
+                            "payment_type_id" => "42dd2a55-6f40-11ea-bde9-1269e7c5a22d",
+                            "paid_at" => "2020-06-10T19:16:46Z"
+                        ]
+                    ]
+                        ])
             ]);
-            dd(Order::all());
-            $request->session()->flash('message', 'Thank you for submitting your proposal. We wil get back to you soon');
-            return redirect('/');
+    } catch (\Throwable $th) {
+        dd($th);
+    }
+            
     }
 
     public function subscribe(Request $request)
     {
-            $request->session()->flash('message', 'You have succesfully subscribed to our news letter');
-            return redirect('/');
+        $request->session()->flash('message', 'You have succesfully subscribed to our news letter');
+        return redirect('/');
     }
 
     public function contact(Request $request)
